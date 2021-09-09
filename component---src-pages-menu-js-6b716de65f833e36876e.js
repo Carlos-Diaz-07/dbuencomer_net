3bcba5f82f0082c979b849b72ced355ed1c6b9aa
carlos-diaz-07 <diazmelian.carlos@gmail.com>
2021-09-09 08:38:30 +0100

    Updates

---
+++ component---src-pages-menu-js-6b716de65f833e36876e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[675],{3418:function(e,t,a){a.r(t);var l=a(7294),n=a(5444),r=a(5669),u=a(3751);t.default=function(){return l.createElement(r.Z,null,l.createElement(u.Z,{title:"Menu"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-menu-js-6b716de65f833e36876e.js.map
